24e81ea3e03f54cc034f7a015f0e2536092ce0f5
DiKhang <dikhang4study@gmail.com>
2022-12-27 17:30:43 +0700

    domain

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const store = require("./store.config.json")
 module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
+  images: {
+    domains: [
+      "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      "localhost",
+      "https://medusa-front.onrender.com",
+    ],
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -17,3 +24,5 @@ module.exports = withStoreConfig({
     ignoreBuildErrors: true,
   },
 })
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2))
